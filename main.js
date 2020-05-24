c062c8c8ea42bb49e43844e315e78dcdc21afd83
Rúnar Berg <runar.BaugssonSigridarson@netapp.com>
2020-05-24 15:45:17 -0700

    Save board posistion in URL

--- main.js
+++ main.js
@@ -4,11 +4,9 @@ import formatSGF from "./formats/sgf.js";
 import drawSVG from "./formats/svg.js";
 
 function createSGFFile(downloadAnchor, sgf) {
-  const sgfFile = new File(
-    [sgf],
-    "random-start.sgf",
-   {      type: "application/x-go-sgf",    }
-  );
+  const sgfFile = new File([sgf], "random-start.sgf", {
+    type: "application/x-go-sgf",
+  });
 
   if (downloadAnchor.href) {
     URL.revokeObjectURL(downloadAnchor.href);
@@ -18,49 +16,129 @@ function createSGFFile(downloadAnchor, sgf) {
   downloadAnchor.download = sgfFile.name;
 }
 
+function formatSearchParams(placements, formData) {
+  return new URLSearchParams([
+    ...Object.entries(formData),
+    [
+      "placements",
+      placements
+        .map(
+          ({ col, row, player }) =>
+            `${player}[${String.fromCharCode(0x60 + col)}${String.fromCharCode(
+              0x60 + row
+            )}]`
+        )
+        .join(""),
+    ],
+  ]);
+}
+
+function setOutput(output, placements, data) {
+  const outputData = output.querySelector("data");
+  const outputPre = output.querySelector("pre");
+
+  outputData.value = JSON.stringify({ ...data, placements });
+  outputPre.textContent = formatText(placements);
+
+  drawSVG(document.getElementById("demo-board"), placements, data);
+
+  createSGFFile(
+    document.getElementById("download-anchor"),
+    formatSGF(placements, data)
+  );
+}
+
+function getFormData(form) {
+  return {
+    stones: form.stones.valueAsNumber,
+    size: form.size.valueAsNumber,
+    komi: form.komi.valueAsNumber,
+    handicap: form.handicap.valueAsNumber,
+    margins: form.margins.valueAsNumber,
+    preventAdjacent: form.preventAdjacent.checked,
+    quadrantShuffle: form.quadrantShuffle.checked,
+  };
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const formData = getFormData(form);
+
+  const placements = randomPlacements(
+    formData.handicap + formData.stones * 2,
+    formData
+  );
+
+  setOutput(form.placements, placements, formData);
+
+  window.history.replaceState(
+    null,
+    "",
+    `?${formatSearchParams(placements, formData)}`
+  );
+}
+
+// Fix the komi in sgf file in change
+function handleKomiChange(event) {
+  const { form } = event.target;
+  const output = form.placements;
+  const outputData = output.querySelector("data");
+
+  if (!outputData.value) {
+    return;
+  }
+
+  const { placements, ...data } = JSON.parse(outputData.value);
+
+  data.komi = form.komi.valueAsNumber;
+  outputData.value = JSON.stringify({ ...data, placements });
+
+  createSGFFile(
+    document.getElementById("download-anchor"),
+    formatSGF(placements, data)
+  );
+
+  window.history.replaceState(
+    null,
+    "",
+    `?${formatSearchParams(placements, data)}`
+  );
+}
+
 function main() {
+  const { searchParams } = new URL(window.location);
   const form = document.getElementById("generator-form");
-  const svg = document.getElementById("demo-board");
-  const outputData = document.getElementById("output-data");
-  const placementsPre = document.getElementById("placements");
-  const downloadAnchor = document.getElementById("download-anchor");
+  let placements;
 
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  for (const [name, value] of searchParams) {
+    if (name === "placements") {
+      placements = value
+        .split("]")
+        .slice(0, -1)
+        .map((str) => {
+          const [player, coord] = str.split("[");
 
-    const formData = {
-      stones: form.stones.valueAsNumber,
-      size: form.size.valueAsNumber,
-      komi: form.komi.valueAsNumber,
-      handicap: form.handicap.valueAsNumber,
-      margins: form.margins.valueAsNumber,
-      preventAdjacent: form.preventAdjacent.checked,
-      quadrantShuffle: form.quadrantShuffle.checked,
-    };
-
-    const placements = randomPlacements(
-      formData.handicap + formData.stones * 2,
-      formData
-    );
-
-    outputData.value = JSON.stringify({ ...formData, placements });
-    placementsPre.textContent = formatText(placements);
-    drawSVG(svg, placements, formData);
-    createSGFFile(downloadAnchor, formatSGF(placements, formData));
-  });
-
-  // Fix the komi in sgf file in change
-  form.komi.addEventListener('change', () => {
-    if (!outputData.value) {
-      return;
+          return {
+            player,
+            col: coord.codePointAt(0) - 0x60,
+            row: coord.codePointAt(1) - 0x60,
+          };
+        });
+    } else if (form[name].type === "checkbox") {
+      form[name].checked = value === "true";
+    } else {
+      form[name].value = value;
     }
 
-    const { placements, ...data } = JSON.parse(outputData.value);
-    data.komi = form.komi.valueAsNumber;
+    if (placements) {
+      setOutput(form.placements, placements, getFormData(form));
+    }
+  }
 
-    outputData.value = JSON.stringify({ ...data, placements });
-    createSGFFile(downloadAnchor, formatSGF(placements, data));
-  });
+  form.addEventListener("submit", handleSubmit);
+  form.komi.addEventListener("change", handleKomiChange);
 }
 
 document.addEventListener("DOMContentLoaded", main);
