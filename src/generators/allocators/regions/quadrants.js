92d08de5c4cc66459e035721935f340a730c6e10
sabu36 <>
2020-09-03 22:16:24 +0900

    Implement allocator 'quadrants'

--- src/generators/allocators/regions/quadrants.js
+++ src/generators/allocators/regions/quadrants.js
@@ -7,6 +7,7 @@
  */
 
 import Grid from "../../utils/grid.js";
+import { regionRect, distanceToBoundaryMaker } from "../../utils/weights.js";
 
 /**
  * @param { number } numStone
@@ -14,13 +15,59 @@ import Grid from "../../utils/grid.js";
  * @param { boolean } needWeights
  * @returns { Allocation }
  */
-export default function quadrants(numStone, config, needWeights = false) {
-  /** @type { Rectangle[] } */
-  const elements = [];
+export default function quadrants(
+  numStone,
+  { size, margins },
+  needWeights = false,
+) {
+  /** @type { Point } */
+  const start = [0, 0];
+  /** @type { Point } */
+  const end = [size, size];
+  const centerLower = Math.floor((size - 1) / 2);
+  const centerUpper = Math.ceil((size - 1) / 2);
 
-  if (needWeights) {
-    return { elements, weights: new Grid([0, 0], [1, 1]) };
+  /** @type { Rectangle[] } */
+  const elements = [
+    [start, [centerLower + 1, centerLower + 1]],
+    [
+      [0, centerUpper],
+      [centerLower + 1, size],
+    ],
+    [
+      [centerUpper, 0],
+      [size, centerLower + 1],
+    ],
+    [[centerUpper, centerUpper], end],
+  ];
+
+  if (!needWeights) {
+    return { elements, weights: null };
   }
 
-  return { elements, weights: null };
+  const distanceToBoundary = distanceToBoundaryMaker(start, end);
+
+  const weights = new Grid(start, end).applyAt(
+    () => 0,
+    regionRect(start, end).filter(
+      (point) => distanceToBoundary(point) <= margins,
+    ),
+  );
+
+  if (centerLower !== centerUpper) {
+    return { elements, weights };
+  }
+
+  return {
+    elements,
+    weights: weights
+      .applyExcept(
+        (val) => 2 * val,
+        regionRect([0, centerLower], [size, centerLower + 1]),
+      )
+      .applyExcept(
+        (val) => 2 * val,
+        regionRect([centerLower, 0], [centerLower + 1, size]),
+      ),
+  };
 }
