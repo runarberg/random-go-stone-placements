c11828426ebda434ae77512ad9f116246507291d
sabu36 <>
2020-09-06 14:48:39 +0900

    Flatten inside directory 'allocators'

---
+++ src/allocators/index.js
@@ -0,0 +1,27 @@
+import whole from "./whole.js";
+import stars from "./stars.js";
+import dominoes from "./dominoes.js";
+import quadrants from "./quadrants.js";
+
+/**
+ * @typedef { import("../../../main.js").Config } Config
+ *
+ * @typedef { [number, number] } Point
+ * @typedef { [Point, Point] } Rectangle
+ * @typedef { Point[] | Rectangle[] } Allocation
+ *
+ * @typedef { "whole" | "stars" | "quadrants" | "dominoes" } Allocator
+ */
+
+// eslint-disable-next-line jsdoc/valid-types
+/*
+ * @type { { [name in Allocator]: (totalStones: number, config: Config) => Allocation } }
+ */
+const allocators = {
+  whole,
+  stars,
+  quadrants,
+  dominoes,
+};
+
+export default allocators;
