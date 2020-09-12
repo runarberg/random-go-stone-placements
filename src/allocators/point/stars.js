bf98b8f6daa54eee73713971bd410a8040fa87aa
sabu36 <>
2020-09-13 01:00:42 +0900

    Only support number of stones <= 9

--- src/allocators/point/stars.js
+++ src/allocators/point/stars.js
@@ -1,5 +1,5 @@
 /**
- * @typedef { import("../../../main.js").Config } Config
+ * @typedef { import("../../main.js").Config } Config
  *
  * @typedef { [number, number] } Point
  */
@@ -18,11 +18,11 @@ const starsAll = [
 ];
 
 /**
- * @param { number } totalStones
  * @param { Config } config
+ * @param { number } totalStones
  * @returns { Point[] }
  */
-export default function stars(totalStones, { size }) {
+export default function stars({ size }, totalStones) {
   if (size !== 19) {
     throw new Error("only works for board size: 19");
   }
@@ -35,11 +35,11 @@ export default function stars(totalStones, { size }) {
     case 5:
     case 7:
     case 9:
-    default:
-      // default covers larger than 9; tentative
       return starsAll.slice(0, totalStones);
     case 6:
     case 8:
       return starsAll.slice(0, 4).concat(starsAll.slice(5, totalStones + 1));
+    default:
+      throw new Error("this allocator only works for 9 or less stones");
   }
 }
--- src/allocators/point/test/stars.test.js
+++ src/allocators/point/test/stars.test.js
@@ -3,13 +3,12 @@ import test from "ava";
 import stars from "../stars.js";
 
 /**
- * @typedef { import("../../../../main.js").Config } Config
+ * @typedef { import("../../main.js").Config } Config
  */
 
-// npm test "./src/generators/allocators/points/test/stars.test.js"
+// npm test "./src/allocators/point/test/stars.test.js"  //
 
 test("stars", (t) => {
-  // only 'size' is used
   /** @type { Config } */
   const config = {
     stones: 0,
@@ -18,10 +17,15 @@ test("stars", (t) => {
     handicap: 0,
     margins: 0,
     preventAdjacent: false,
-    generator: "uniform",
+    allocatorType: "point",
+    allocatorPoint: "stars",
+    allocatorRect: "whole",
+    placerPoint: "pointsUnaltered",
+    placerRect: "distUniform",
+    weightAdjuster: "constant",
   };
 
-  t.deepEqual(stars(5, config), [
+  t.deepEqual(stars(config, 5), [
     [3, 15],
     [15, 3],
     [15, 15],
@@ -29,7 +33,7 @@ test("stars", (t) => {
     [9, 9],
   ]);
 
-  t.deepEqual(stars(8, config), [
+  t.deepEqual(stars(config, 8), [
     [3, 15],
     [15, 3],
     [15, 15],
@@ -40,7 +44,7 @@ test("stars", (t) => {
     [15, 9],
   ]);
 
-  t.deepEqual(stars(12, config), [
+  t.deepEqual(stars(config, 9), [
     [3, 15],
     [15, 3],
     [15, 15],
