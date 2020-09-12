49083f2fa60504f9b2db56b1f2fc0f1b8d9798b8
sabu36 <>
2020-09-13 03:38:40 +0900

    Implement dist-uniform.js based on uniform.js

--- src/placers/rectangle/dist-uniform.js
+++ src/placers/rectangle/dist-uniform.js
@@ -1,57 +1,27 @@
 /**
- * @typedef { import("../main.js").Placement } Placement
- * @typedef { import("../main.js").Config } Config
- */
-
-import { getNextPlayer, allowedCoord } from "./utils.js";
-
-/**
- * @param { object } obj
- * @param { number } obj.start
- * @param { number } obj.end
- * @returns { number }
- */
-function randomPos({ start = 3, end = 17 }) {
-  return start + Math.floor(Math.random() * (end - start) + 1);
-}
-
-/**
- * Create a new random placement
+ * @typedef { import("../../main.js").Config } Config
  *
- * @param { Placement[] } placements
- * @param { Config } config
- * @returns { Placement }
+ * @typedef { [number, number] } Point
+ * @typedef { [Point, Point] } Rectangle
  */
-function newCoord(placements, config) {
-  const { size, margins, handicap, preventAdjacent } = config;
 
-  const start = margins + 1;
-  const end = size - margins;
-  const col = randomPos({ start, end });
-  const row = randomPos({ start, end });
-
-  if (!allowedCoord([col, row], placements, { preventAdjacent })) {
-    return newCoord(placements, config);
-  }
-
-  const player = getNextPlayer(placements, handicap);
-
-  return { col, row, player };
-}
+import { pickUniformRect, allowedCoord } from "../../utils/prob-dist.js";
 
 /**
- * @typedef { { placements: Placement[] } } State
- * @param { number } n
  * @param { Config } config
- * @param { State | undefined } state
- * @returns { Placement[] }
+ * @param { Rectangle[] } allocation
+ * @returns { Point[] }
  */
-export default function uniform(n, config, state = { placements: [] }) {
-  if (n <= 0) {
-    return state.placements;
-  }
+export default function distUniform(config, allocation) {
+  return allocation.reduce((/** @type { Point[] } */ stones, [start, end]) => {
+    /** @type { Point } */
+    let stn;
 
-  state.placements.push(newCoord(state.placements, config));
+    do {
+      stn = pickUniformRect(start, end);
+    } while (!allowedCoord(stn, stones, config));
 
-  return uniform(n - 1, config, state);
+    stones.push(stn);
+    return stones;
+  }, []);
 }
