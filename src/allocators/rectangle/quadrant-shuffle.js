89fa13962f2d3c50a40cc80d1364c24c93a67e23
sabu36 <>
2020-09-13 02:20:38 +0900

    Move getRandomQuadrant() unchanged from quadrant-shuffle.js into quadrants.js

--- src/allocators/rectangle/quadrant-shuffle.js
+++ src/allocators/rectangle/quadrant-shuffle.js
@@ -11,38 +11,6 @@
 import { allowedCoord, getNextPlayer } from "./utils.js";
 
 /**
- * @param { number } quadrantMask
- * @returns { number }
- */
-function getRandomQuadrant(quadrantMask) {
-  const bits = Array.from(quadrantMask.toString(2).padStart(4, "0"), (n) =>
-    Number.parseInt(n, 10),
-  ).reverse();
-  const once = bits.reduce((count, bit) => count + bit, 0);
-
-  if (once === 1) {
-    return quadrantMask;
-  }
-
-  // Get a random nth of the available bits.
-  const nth = Math.floor(Math.random() * once);
-  let bitsLeft = nth + 1;
-  const index = bits.findIndex((bit) => {
-    if (bit) {
-      bitsLeft -= 1;
-    }
-
-    if (bitsLeft === 0) {
-      return true;
-    }
-
-    return false;
-  });
-
-  return 1 << index;
-}
-
-/**
  * @param { number } n
  * @param { Config } config
  * @param { State | undefined } state
--- src/allocators/rectangle/quadrants.js
+++ src/allocators/rectangle/quadrants.js
@@ -9,6 +9,38 @@ import Grid from "../../utils/grid.js";
 import { regionRect } from "../../utils/weights.js";
 
 /**
+ * @param { number } quadrantMask
+ * @returns { number }
+ */
+function getRandomQuadrant(quadrantMask) {
+  const bits = Array.from(quadrantMask.toString(2).padStart(4, "0"), (n) =>
+    Number.parseInt(n, 10),
+  ).reverse();
+  const once = bits.reduce((count, bit) => count + bit, 0);
+
+  if (once === 1) {
+    return quadrantMask;
+  }
+
+  // Get a random nth of the available bits.
+  const nth = Math.floor(Math.random() * once);
+  let bitsLeft = nth + 1;
+  const index = bits.findIndex((bit) => {
+    if (bit) {
+      bitsLeft -= 1;
+    }
+
+    if (bitsLeft === 0) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return 1 << index;
+}
+
+/**
  * @param { number } size
  * @param { Rectangle[] } rectangles
  * @returns { Grid }
